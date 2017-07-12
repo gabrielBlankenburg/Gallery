ef6e85473c2042da83ae0ebd5cd999887ac584d1
gabrielBlankenburg <gabriel.blankenburg.p.silva@gmail.com>
2017-07-12 01:41:53 -0300

    Created the homepage of the gallery

---
+++ javascript/main.js
@@ -0,0 +1,51 @@
+$(document).ready(function(){
+	$('#openNav').click(function(){
+		openNav();
+	});
+	$('#closeNav').click(function(){
+		closeNav();
+	});
+	$('#cover-page').click(function(){
+		closeNav();
+	});
+	featured();
+
+});
+
+// Open the menu, hide the openNav id and shows the closeNav id
+function openNav(){
+	$('#openNav').removeClass('fadingIn fadingOut').addClass('fadingOut');
+	$('#closeNav').removeClass('fadingIn fadingOut').addClass('fadingIn');
+	$('#main-menu').show("slide", { direction: "right" }, 500);
+	$('header, main, footer').animate({opacity: '.2'}, 500);
+	$('#cover-page').css('display', 'block');
+	
+
+}
+
+// Close the menu, hide the closeNav id and shows the openNav id
+function closeNav(){
+	$('#closeNav').removeClass('fadingIn fadingOut').addClass('fadingOut');
+	$('#openNav').removeClass('fadingIn fadingOut').addClass('fadingIn');
+	$('#main-menu').hide("slide", { direction: "right" }, 500);
+	$('header, main, footer').animate({opacity: '1'}, 500);
+	$('#cover-page').css('display', 'none');
+}
+
+
+// This makes the elements inside featured class get the opacity equals 1 and appear the text on hover
+function featured(){
+	var opacity = '' + $('.featured').children('span').css('opacity');
+	var padding = '' + $('.featured').children('span').children('img').css('padding');
+	var display = '' + $('.featured').children('span.hidden-text').children('p').css('display');
+	$('.featured').hover(function(){
+		$(this).children('span').css('opacity', '1').css('transition', '.5s');
+		$(this).children('span').children('img').css('padding', '0').css('transition', '.5s');
+		$(this).children('span.hidden-text').children('p').css('display', 'block').css('transition', '.5s');
+	}, function(){
+		$(this).children('span').css('opacity', opacity).css('transition', '.5s');
+		$(this).children('span').children('img').css('padding', padding).css('transition', '.5s');
+		$(this).children('span.hidden-text').children('p').css('display', display).css('transition', '.5s');
+	});
+}
+
